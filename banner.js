c305dface981722a9a9ab9b427da2498c5409ff7
BG Music <78909975+BG-Music@users.noreply.github.com>
2024-06-14 22:02:40 +0100

    Update banner.js

--- banner.js
+++ banner.js
@@ -2,15 +2,15 @@ let showBanner = 1
 
 if (showBanner === 1) {
     
-const bannerText = "This is an test"
+const bannerText = "This is an test, no button banner"
 const buttonText = "View"
-const buttonURL = "/birthday"
+const buttonURL = "none"
 const backgroundColor = `orange`
 const textColor = 'black'
 
 if (buttonURL === "none") {
 document.write(`
-<div style="background-color: ${backgroundColor}; width: 100%; height: 7%; position: sticky; box-shadow: 2px 10px 8px 5px ${backgroundColor}" >
+<div style="background-color: ${backgroundColor}; width: 100%; height: 7%; position: sticky; box-shadow: 1px 4px 4px 4px ${backgroundColor}" >
     <h4 style="display: inline-block; text-align: center; width: 100%;" >
         <p style="display: inline; color: ${textColor}">${bannerText}</p>
     </h4>
@@ -33,7 +33,7 @@ filter: brightness(90%);
 }
 </style>
 
-<div style="background-color: ${backgroundColor}; width: 100%; height: 7%; position: sticky; box-shadow: 2px 10px 8px 5px ${backgroundColor}" >
+<div style="background-color: ${backgroundColor}; width: 100%; height: 7%; position: sticky; box-shadow: 1px 4px 4px 4px ${backgroundColor}" >
     <h4 style="display: inline-block; text-align: center; width: 100%;" >
         <p style="display: inline; color: ${textColor}">${bannerText}</p> 
         <a href="${buttonURL}" style="color: ${textColor}; background-color: ${backgroundColor}; border-radius: 50px; padding: 10px; float: right;" class="linkButton">${buttonText}</a>
